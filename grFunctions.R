6dcf29750b7e2d8b49c476b1605c52ffb693d6d7
agranado <alejandro.granados.c@gmail.com>
2018-11-05 23:04:41 -0800

    Latest update patski scripts

--- grFunctions.R
+++ grFunctions.R
@@ -7,7 +7,7 @@ grToDataFrame<-function(chronis.atac){
   celln1<-data.frame( seqnames=seqnames(chronis.atac),start=start(chronis.atac)-1,
                       end=end(chronis.atac),
                       scores=c(rep(open.val,length(chronis.atac))),type=c(rep("Intergenic",length(chronis.score))))
-  #Rename the colums such that my old script works 
+  #Rename the colums such that my old script works
   names(celln1)<-c("chr","start","end","score","type")
   #define the levels 0->closed; 1->open
   levels(celln1$score)<-c(0,1)
@@ -15,22 +15,44 @@ grToDataFrame<-function(chronis.atac){
 }
 
 
+#same function as before but when the GR object has actual numerical scores
+grToDataFrameScore<-function(chronis.atac){
+  closed.val = 0
+  open.val=1
+  chronis.score = rep(1,length(chronis.atac))
+  #make data frame with same info (helps to process the data)
+  celln1<-data.frame( seqnames=seqnames(chronis.atac),start=start(chronis.atac)-1,
+                      end=end(chronis.atac),
+                      score=chronis.atac$score,type=c(rep("Intergenic",length(chronis.score))))
+  #Rename the colums such that my old script works
+  names(celln1)<-c("chr","start","end","score","type")
+  #define the levels 0->closed; 1->open
+  #levels(celln1$score)<-c(0,1)
+  return(celln1)
+}
+
+
+#modified for X patski cell line analysis:
+#this is part of an independent GIT repository for anlaysis of patski cell line
+
+#major changes:
+#   Add new function grToDataFrameScore that actually includes the numerical score provided in the original data
 segment_genome<-function(all.atac,window.size){
   #all.atac is a list of GR objects
- 
+
   celln1.list=list()
-  all.chr = levels(seqnames(all.atac[[1]])) 
-   
+  all.chr = levels(seqnames(all.atac[[1]]))
+
 
     starts.chr = matrix(0,length(all.atac),length(all.chr))
     end.chr= matrix(0,length(all.atac),length(all.chr))
-    
+
     #Here we will generate a matrix of data.frames, each one from an ATAC seq dataset
     #all the samples in the list will be clustered in the future, so we need to normalize their length
     for(i in 1:length(all.atac)){
       #convert each ATAC dataset to a dataframe
-      celln1.list[[i]]<-grToDataFrame(all.atac[[i]])
-      
+      celln1.list[[i]]<-grToDataFrameScore(all.atac[[i]])
+
       celln1.gr=all.atac[[i]]
       all.chr=levels(seqnames(celln1.gr))
       #lets find out the range of each chromosome for each data set
@@ -41,82 +63,98 @@ segment_genome<-function(all.atac,window.size){
         end.chr[i,c] = max(celln1$end[celln1$chr == chr  ]  )
       }
     }
-    
-    #look for the max value acrros the starting points: 
-    #we need to start all samples at the region in the chromosome, 
+
+    #look for the max value acrros the starting points:
+    #we need to start all samples at the region in the chromosome,
     #so we need to start from the one with the highest value
     max.starts = apply(starts.chr,2,max)
     min.ends = apply(end.chr,2,min)
-    
-    
+
+
     #now that we have the range of the samples we can partition the genome in contigous 10kb windows
-    
-    
+
+
     all.segmented.gr = list()
     #lets go through all the datasets in the list,
-     for(i in 1:length(all.atac)){   
+     for(i in 1:length(all.atac)){
           all.chr=levels(seqnames(all.atac[[i]]))
           celln1 = celln1.list[[i]]
           all.seq.names = array()
           all.new.starts= array()
           all.new.ends = array()
           all.segmented = array()
-          
+          all.scores= array()
+
           for(c in 1:length(all.chr)){
                 chr=all.chr[c]
                 #how many windows of 10kb do we have in this chromosome
                 segmented.chr.size=round((min.ends[c] - max.starts[c])/window.size)
-                #the new array 
+                #the new array
                 segmented.chromosome =array(0,segmented.chr.size)
                 new.starts = array(0,segmented.chr.size)
                 new.ends= array(0,segmented.chr.size)
-     
+                new.scores = array(0,segmented.chr.size)
+
                 #get all the start / end regions from this chromosome
                 st.chr=celln1$start[celln1$chr==chr]
                 en.chr = celln1$end[celln1$chr==chr]
-                
+                score.chr = celln1$score[celln1$chr==chr]
+
                 init.region =max.starts[c]
                 end.region = max.starts[c] + window.size +1
-                
+
                 for(s in 1:length(segmented.chromosome)){
-    
+                    #Here en.chr includes all the endpoints of the regions (peaks) in the original data.
+                    # segmented.chromosome will COUNT the total number of peaks within each NEW region of the
+                    # segmented data ( by window.size)
+
+                    #if the data had score we could include them here:
                     segmented.chromosome[s] = sum(en.chr<= end.region & st.chr>= init.region)
+                    #new values: # # # # # #
                     new.starts[s] = init.region
                     new.ends[s] = end.region
-                      
+                    #since we are mergin peaks, lets's
+                    #take the scores for all peaks in this regions and mutiplied by their values
+                    #then add them as weighted sum
+                    if(segmented.chromosome[s]>0){
+                      new.scores[s] = sum(score.chr[en.chr<= end.region & st.chr>= init.region])
+                    }
+                    # end new values # # # # # #
+
+                    #update indexes:
                     init.region = init.region + window.size +1
                     end.region = init.region + window.size
                 }
                 seq.names=rep(chr,length(segmented.chromosome))
-                
+
                 all.seq.names = c(all.seq.names,seq.names)
                 all.new.starts = c(all.new.starts,new.starts)
                 all.new.ends = c(all.new.ends, new.ends)
                 all.segmented = c(all.segmented,segmented.chromosome)
- 
+                all.scores = c(all.scores, new.scores)
           }
-          segmented.dataframe=data.frame(chr=all.seq.names,start=all.new.starts,end=all.new.ends,score=all.segmented)
+          segmented.dataframe=data.frame(chr=all.seq.names,start=all.new.starts,end=all.new.ends,score=all.scores,npeaks = all.segmented)
           segmented.dataframe=segmented.dataframe[!is.na(segmented.dataframe$start),]
           segmented.gr=makeGRangesFromDataFrame(segmented.dataframe,ignore.strand=T,seqnames.field="chr",keep.extra.columns=T)
-          
+
           all.segmented.gr[[i]]=segmented.gr
-     }  
-    
-    
-    
+     }
+
+
+
     return(all.segmented.gr)
-    
-    
-    
+
+
+
 }
 
 
 cluster_chromosome<-function(all.segmented.gr,chr){
-  
+
   chr.matrix=c()
   for(l in 1:length(all.segmented.gr)){
         this.chr=all.segmented.gr[[l]][ seqnames(all.segmented.gr[[l]])==chr  ]$score
         chr.matrix = cbind(chr.matrix,this.chr)
     }
-  
+
 }
--- patskiAnalysis.R
+++ patskiAnalysis.R
@@ -9,7 +9,7 @@ library(rtracklayer)
 library(IdeoViz)
 library(GenomicRanges)
 library(readxl)
-
+setwd("/home/alejandrog/MEGA/Caltech/epimemoir/chromatin") #GIT repository
 patski= import.bed("patskiWT.allelicATAC.dScore.minCov_5.bedgraph.gz")
 
 
@@ -28,18 +28,35 @@ patski.df = data.frame(chr = seqnames(patski), start = start(ranges(patski)),
 patski.gr=makeGRangesFromDataFrame(patski.df,ignore.strand = T,
                                     seqnames.field = "chr",keep.extra.columns = T)
 
+#There are some repetead entries in the data
+patski.gr.unique = unique(patski.gr)
 #Let's partition the genome into equal length long regions
 #We can use a custom function
 #input argument is a list of bed objects
 all.bed.files  = list()
-all.bed.files[[1]] = patski.gr #only element in the list
+all.bed.files[[1]] = patski.gr.unique #only element in the list
 
 #Load the function :
 source("grFunctions.R")
 
 #return a genomic ranges object
+#We still need to modify this
+
+#partition the genome in windows to look for density of atac peaks
 window.size=5000
 all.segmented.gr<-segment_genome(all.bed.files,window.size)
 
-patski.gr = unique(patski.gr)
-patski.X = patski.gr[ seqnamse(patski.gr)=="chrX" ]
+#let's take only the ATAC peaks from the X chromosome
+patski.X = patski.gr.unique[ seqnames(patski.gr.unique)=="chrX" ]
+#WE can se how many sites we for a give score
+# There are 1605 peaks in the X chromose of which 204 have score 0.5 (maximum)
+#Sushi library
+library(Sushi)
+x<-data.frame(chrom = seqnames(patski.X.segmented),start = start(patski.X.segmented),
+            end = end(patski.X.segmented),value = patski.X.segmented$score)
+
+chrom = "chrX";chromstart = min(x$start);chromend=max(x$end)
+plotBedgraph(x,chrom = "chrX",chromstart = min(x$start),chromend = max(x$end))
+labelgenome(chrom,chromstart,chromend,n=4,scale="Mb")
+mtext("ATAC d-score",side=2,line=1.75,cex=1,font=2)
+axis(side=2,las=2,tcl=.2)
